337f6bb2957f6dbecadbf9fc1adb0d58ec9d4cd0
Minsu Han <mshanobject@gmail.com>
2019-12-10 17:27:51 +0900

    complete js,css for m_signup_account.html

--- javascript/parkingplay/js/ui.js
+++ javascript/parkingplay/js/ui.js
@@ -3,15 +3,19 @@ document.addEventListener("DOMContentLoaded", function () {
     var pwCheckBtn = document.querySelectorAll('button.pw_check');
     addEventFunc(pwCheckBtn, 'click', showPassword);
 
-    var inputField = document.querySelectorAll('input[class=field]');
-    addEventFunc(inputField, 'keyup', checkInputFieldValid);
-    addEventFunc(inputField, 'focusout', checkInputFieldValid);
+    var inputFields = document.querySelectorAll('input[class=field]');
+    // addEventFunc(inputFields, 'keyup', checkInputFieldValid);
+    addEventFunc(inputFields, 'input', checkInputFieldValid);
+    addEventFunc(inputFields, 'focusout', checkInputFieldValid);
 
     var checkAllInput = document.getElementById('ck_all');
     var requiredCheckboxes = document.querySelectorAll('input[type=checkbox][required]');
     
     var numberOfCheckedRequiredCheckboxes = countCheckedboxes(requiredCheckboxes);
     addEventFunc(requiredCheckboxes, 'click', function() {
+        if (requiredCheckboxes == null || requiredCheckboxes == undefined) {
+            return;
+        }
         if (event.target.checked === false) {
             checkAllInput.checked = false;
             numberOfCheckedRequiredCheckboxes--;
@@ -24,6 +28,9 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
     addEventFunc(checkAllInput, 'change', function () {
+        if (checkAllInput == null || checkAllInput == undefined) {
+            return;
+        }
         if (event.target.checked != true) {
             checkAll(requiredCheckboxes, false);
             numberOfCheckedRequiredCheckboxes = 0;
@@ -45,20 +52,19 @@ function countCheckedboxes(checkboxes) {
     return counts;
 }
 
-function addEventFunc(element, eventName, myFunc) {
-    var elementLength = element.length;
-    if (element == null || element == undefined || elementLength == 0) {
+function addEventFunc(elements, eventName, myFunc) {
+    if (elements == null || elements == undefined || elements.length == 0) {
         return;
     }
     
-    if (elementLength == undefined) {
-        element.addEventListener(eventName, myFunc);
+    if (elements.length == undefined) {
+        elements.addEventListener(eventName, myFunc);
         return;
     }
 
-    for (var i=0; i < elementLength; i++) {
-        element[i].addEventListener(eventName, myFunc);
-    }
+    elements.forEach(function(element) {
+        element.addEventListener(eventName, myFunc);
+    });
 }
 
 function checkAll(checkboxes, truefalse) {
@@ -70,6 +76,9 @@ function checkAll(checkboxes, truefalse) {
 }
 
 function showPassword() {
+    if (this == null || this == undefined) {
+        return;
+    }
     changeDbt(this, 'show_password', 'hide_password');
     var targetElement = document.getElementById('password');
     changeInputType(targetElement, 'password', 'text');
@@ -114,6 +123,9 @@ function changeInnerHTML(element, msg) {
 }
 
 function checkInputFieldValid() {
+    if (this == null || this == undefined) {
+        return;
+    }
     var targetValue = this.value;
 
     if (targetValue.length < 0) {
@@ -123,34 +135,127 @@ function checkInputFieldValid() {
     if (targetValue.length == 0) {
         replaceClassName(this, 'warning', '');
         replaceClassName(this, 'complete', '');
+        removeTipElement(this);
+        classNameHandler(this.parentElement, 'tip_space', '');
         return;
     }
     
     var msg = '';
+    
     switch (this.name) {
 
         case 'id':
             if (!RegExp(/^[A-Za-z0-9]{6,20}$/).test(targetValue)) {
                 msg = '아이디를 6~20자의 영문, 숫자로 입력해 주세요.';
                 classNameHandler(this, 'complete', 'warning');
+                addClassName(this.parentElement, 'tip_space');
             } else {
                 msg = '';
                 classNameHandler(this, 'warning', 'complete');
+                classNameHandler(this.parentElement, 'tip_space', '');
             }
+            tipMsgHandler(this, msg);
             break;
         case 'password':
             if (!RegExp(/^\S{8,20}$/).test(targetValue)) {
-                msg = '비밀번호를 8~20자의 영문, 숫자, 특수문자 등으로 입력해 주세요. 공백은 사용할 수 없습니다.';
+                msg = '비밀번호를 8~20자의 영문, 숫자, 특수문자 등으로 입력해 주세요.\n 공백은 사용할 수 없습니다.';
                 classNameHandler(this, 'complete', 'warning');
-            } else if (this.getAttribute('id') == 'confirm_pw' && this.value != document.getElementById('create_pw').value) {
-                msg = '재입력 비밀번호가 비밀번호와 일치하지 않습니다.';
-                classNameHandler(this, 'complete', 'warning');
+                addClassName(this.parentElement, 'tip_space');
             } else {
                 msg = '';
                 classNameHandler(this, 'warning', 'complete');
+                classNameHandler(this.parentElement, 'tip_space', '');
             }
+            tipMsgHandler(this, msg);
+            break;
+        case 'create_id':
+            if (!RegExp(/^[A-Za-z0-9]{6,20}$/).test(targetValue)) {
+                msg = '아이디를 6~20자의 영문, 숫자로 입력해 주세요.';
+                classNameHandler(this, 'complete', 'warning');
+                addClassName(this.parentElement, 'tip_space');
+            } else {
+                msg = '';
+                classNameHandler(this, 'warning', 'complete');
+                classNameHandler(this.parentElement, 'tip_space', '');
+            }
+            tipMsgHandler(this, msg);
+            break;
+        case 'create_pw':
+            if (!RegExp(/^\S{8,20}$/).test(targetValue)) {
+                msg = '비밀번호를 8~20자의 영문, 숫자, 특수문자 등으로 입력해 주세요.\n 공백은 사용할 수 없습니다.';
+                classNameHandler(this, 'complete', 'warning');
+                addClassName(this.parentElement, 'tip_space');
+            } else {
+                msg = '';
+                classNameHandler(this, 'warning', 'complete');
+                classNameHandler(this.parentElement, 'tip_space', '');
+            }
+            tipMsgHandler(this, msg);
+            break;
+        case 'confirm_pw':
+            if (this.value != document.getElementById('create_pw').value) {
+                msg = '재입력 비밀번호가 비밀번호와 일치하지 않습니다.';
+                classNameHandler(this, 'complete', 'warning');
+                addClassName(this.parentElement, 'tip_space');
+            } else {
+                msg = '';
+                classNameHandler(this, 'warning', 'complete');
+                classNameHandler(this.parentElement, 'tip_space', '');
+            }
+            tipMsgHandler(this, msg);
+            break;
+        case 'email':
+            if (!RegExp(/^[\w.-]+@[A-Za-z0-9]([A-Za-z0-9-]{0,61}[A-Za-z0-9])?(\.[A-Za-z0-9]([A-Za-z0-9-]{0,61}[A-Za-z0-9])?)+$/).test(targetValue)) {
+                msg = '이메일을 정확히 입력해 주세요.';
+                classNameHandler(this, 'complete', 'warning');
+                addClassName(this.parentElement, 'tip_space');
+            } else {
+                msg = '';
+                classNameHandler(this, 'warning', 'complete');
+                classNameHandler(this.parentElement, 'tip_space', '');
+            }
+            tipMsgHandler(this, msg);
             break;
     }
+    submitHandler();
+}
+
+function submitHandler(){
+    var requiredInputFields = document.querySelectorAll('input.field[required]');
+    var completedInputs = 0;
+    requiredInputFields.forEach(function(element) {
+        var classNameArray = element.className;
+        if (classNameArray.indexOf('complete') != -1) {
+            completedInputs++;
+        }
+    });
+    var submit = document.querySelector('input[type=submit]');
+    if (completedInputs == requiredInputFields.length){
+        submit.disabled = false;
+    } else {
+        submit.disabled = true;
+    }
+}
+
+function removeTipElement(inputElement) {
+    var tip = inputElement.parentElement.lastChild;
+    if (tip.className == 'tip'){
+        tip.remove();
+    }
+}
+
+function tipMsgHandler(element, message) {
+    var tip = element.parentElement.lastChild;
+    var newMsgNode = document.createTextNode(message);
+
+    if (tip.className != 'tip'){
+        tip = document.createElement("p");
+        tip.className += 'tip';
+        tip.appendChild(newMsgNode);
+    } else {
+        tip.innerText = message;
+    }
+    element.parentElement.appendChild(tip);
 }
 
 function classNameHandler(element, removedName, addedName) {
@@ -173,7 +278,7 @@ function replaceClassName(element, origin, changeTo) {
     var originPattern = "(?:^|\\s)" + origin + "(?!\\S)";
     var strChangeTo = changeTo;
     var classNameArray = element.className.split(" ");
-    if (classNameArray.length != 1) {
+    if (classNameArray.length != 1 && changeTo != '') {
         strChangeTo = " " + changeTo;
     }
     var originRex = new RegExp(originPattern, 'g');
