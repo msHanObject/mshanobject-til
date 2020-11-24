71ead7d51b6433037f59dc9ec514d4717d907554
Minsu Han <mshanobject@gmail.com>
2020-11-24 09:50:36 +0900

    add get_row_col function

--- php/phpspreadsheet/xlsx_create.php
+++ php/phpspreadsheet/xlsx_create.php
@@ -7,45 +7,65 @@ use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 $spreadsheet = new Spreadsheet();
 $sheet = $spreadsheet->getActiveSheet();
 
+function get_row_col($cell)
+{
+    $cell_splits = str_split($cell);
+    for ($i = 0; count($cell_splits); $i++) {
+        if (is_numeric($cell_splits[$i])) {
+            $col_char = substr($cell, 0, $i);
+            $row_num = substr($cell, $i);
+            return array($col_char, $row_num);
+        }
+    }
+    return array('A', 1);
+}
+
 $data = array(
     array(
-            "seq" => 60,
+            "seq" => 1,
             "code" => 'https://www.example.com/st/?code=072b030ba126',
             "stamp_name" => 'Stamp A',
             "geopark_name" => '지질Park A',
             "lat" => 35.9960190,
             "lng" => 127.7953070,
-            "visits" => 1
+            "visits" => 10
         ),
     array(
-            "seq" => 59,
+            "seq" => 2,
             "code" => 'https://www.example.com/st/?code=093f65e080a29',
             "stamp_name" => 'Stamp B',
             "geopark_name" => '지질Park B',
             "lat" => 35.8431800,
             "lng" => 127.6825570,
-            "visits" => 2
+            "visits" => 20
         ),
     array(
-            "seq" => 58,
+            "seq" => 3,
             "code" => 'https://www.example.com/st/?code=66f041e16a69',
             "stamp_name" => 'Stamp C',
             "geopark_name" => '지질Park C',
             "lat" => 35.9753270,
             "lng" => 127.4055170,
-            "visits" => 3,
+            "visits" => 30,
         )
 );
-$i = 1;
+$start_cell = 'A1';
+list($col_char, $row_num) = get_row_col($start_cell);
+// Set row header
+$row = array_keys($data[0]);
+foreach ($row as $cell) {
+    $sheet->setCellValue($col_char++ . $row_num, $cell);
+}
+// Set row value
+$row_num = 2;
 foreach ($data as $row) {
-    $row = array_values($row);
-    $c = 'A';
+    $row= array_values($row);
+    $col_char = 'A';
     foreach ($row as $cell) {
-        $sheet->setCellValue($c++.$i, $cell);
+        $sheet->setCellValue($col_char++ . $row_num, $cell);
     }
-    $i++;
+    $row_num++;
 }
 
-$path = 'resource/';
 $writer = new Xlsx($spreadsheet);
-$writer->save($path . 'hello 월드.xlsx');
+$writer->save('파일이름.xlsx');
