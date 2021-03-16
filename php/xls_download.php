73cb213e9299db1367e2c55ba6a0e3ebe6a288dd
Minsu Han <mshanobject@gmail.com>
2021-03-16 22:30:56 +0900

    moved in to create_xls dir

--- php/xls_download.php
+++
@@ -1,9 +0,0 @@
-<?php
-$data = 'aaa,bbb,ccc,ddd,eee
-123,456,789,20394
-한글,출력,엑셀,문제';
-header('Content-Type: application/vnd.ms-excel; charset=UTF-8');
-header('Expires: 0');
-header('Content-Disposition: attachment; filename=test.xls');
-header('Content-Transfer-Encoding: binary');
-echo $data;
